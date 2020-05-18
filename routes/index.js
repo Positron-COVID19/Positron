002be783b610fc45aa7d29d23653aa238aeb5efd
David Liao <liao.canada@gmail.com>
2020-05-18 11:04:14 -0400

    Add route for /news, pass dummy news headlines to pug file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const routes = require("express").Router();
 
+// Handle home page
 routes.get("/", (request, response) => {
     // Renders /views/pages/index.pug, passing in empty parameters
     //     and sends HTTP 200 with the rendered HTML
     response.render("index", {});
 });
+
+// Handle other pages using the other routers
+routes.use("/news", require("./news"));     // NOTE: any route *prefixed* with /news will get
+                                            //     routed to the news router, including /news/123
 
 module.exports = routes;
